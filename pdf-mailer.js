77e886ba41dcc6d978d149f700d99e6275b3768d
praveen-221 <praveen-221>
2022-11-20 00:06:00 +0530

    added mailer automation

--- pdf-mailer.js
+++ pdf-mailer.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
-const receivers = require("./receivers.json");
+// const receivers = require("./receivers.json");
 
 dotenv.config();
 const transport = nodemailer.createTransport({
@@ -12,19 +12,35 @@ const transport = nodemailer.createTransport({
     }
 })
 
-var cnt = 1;
+var cnt = 0;
+const chunkSize = 5;
+for (let i = 0; i < receivers.length; i += chunkSize) {
+    const chunk = receivers.slice(i, i + chunkSize);
+	// var delayInMilliseconds = 10000;
+    chunk.forEach((element, index)=> {
+        if(element.email != ""){
+            setTimeout(()=>{
+                Send(element);
+                console.log(index);
+            }, 5000);
+        }
+    });
+}
 
 const Send = (receiver) => {
     const options = {
-        from: "test_mail@gmail.com",
-        to: receiver.email,
-        subject: "Hello from space",
-        text: "     Hi "+ receiver.name +", If you see this mail then nodemailer works perfectly.\nJust playing buddy :):" + '\n' +
-        "   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        from: process.env.USER,
+        to: receiver.Email,
+        subject: "Prodigy Participation Certificate",
+        text: "Greetings from ACM-CEG! \n\n\tA hearty congratulations to the students who participated in the events! We hope they enjoyed and had fun participating. \nWe would be elated to have you next year too for our next edition of Prodigy. Thank you for your support and participation towards making this event a grand success \n\n\tKindly let us know your feedback and suggestions to make Prodigy better in the future.\nFeedback form: https://forms.gle/zWhZ6dQHFW3TXE9j6 \n\n\nWarm regards,\nACM-CEG.",
+        attachments : [
+			{
+                filename: receiver.ID+'.pdf',
+                path: __dirname+'/E-certificates/'+ receiver.Name+'_'+receiver.ID+'.pdf'
+			}
+		]
     };
     // console.log(options);
-    
-    console.log(`Sending Email: ${cnt}`);
     
     transport.sendMail(options, (err, info)=> {
         if(err){
@@ -34,13 +50,15 @@ const Send = (receiver) => {
             cnt += 1;
         }
     });
+    console.log(`Sending Email to ${receiver.Name}`);
 }
-
-receivers.forEach((element, index)=> {
-    if(element.email != ""){
-        setTimeout(()=>{
-            Send(element);
-            console.log(index);
-        }, element.id * 5000);
-    }
-});
+console.log(`Total Email sent: ${cnt}`);
+// Send();
+// receivers.forEach((element, index)=> {
+//     if(element.email != ""){
+//         setTimeout(()=>{
+//             Send(element);
+//             console.log(index);
+//         }, element.id * 5000);
+//     }
+// });
